34b4628541ed55c6d3531e5c30a28d873345a5dd
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-01-12 12:08:59 +0530

    feat: resend integration for email sending

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,7 @@
 const config = {
   env: {
     apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
+    prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT!,
     imagekit: {
       publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
       urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
@@ -12,6 +13,9 @@ const config = {
       redisToken: process.env.UPSTASH_REDIS_TOKEN!,
       qstashUrl: process.env.UPSTASH_QSTASH_URL!,
       qstashToken: process.env.UPSTASH_QSTASH_TOKEN!,
+    },
+    resend: {
+      token: process.env.RESEND_TOKEN!,
     },
   },
 };
--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,7 +1,33 @@
 import config from "./config";
 import { Client as WorkflowClient } from "@upstash/workflow";
+import { Client as QStashClient, resend } from "@upstash/qstash";
 
 export const workflowClient = new WorkflowClient({
   baseUrl: config.env.upstash.qstashUrl,
   token: config.env.upstash.qstashToken,
 });
+
+const qStashClient = new QStashClient({
+  token: config.env.upstash.qstashToken,
+});
+
+interface EmailParams {
+  email: string;
+  subject: string;
+  message: string;
+}
+
+export const sendEmail = async ({ email, subject, message }: EmailParams) => {
+  await qStashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resend.token }),
+    },
+    body: {
+      from: "Deepanshu <console.deepanshumehra.site>",
+      to: [email],
+      subject,
+      html: message,
+    },
+  });
+};
